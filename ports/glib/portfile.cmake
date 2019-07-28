f3057f2b48fa65b9191ff3e1f65e79fcf4379a3f
Norbert Nemec <Norbert@Nemec-online.de>
2019-07-28 20:59:05 +0200

    [glib] update to 2.60.4 (mesonbuild)

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -10,43 +10,89 @@ if (NOT VCPKG_CMAKE_SYSTEM_NAME)
     vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 endif()
 
-set(GLIB_VERSION 2.52.3)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://ftp.gnome.org/pub/gnome/sources/glib/2.52/glib-${GLIB_VERSION}.tar.xz"
-    FILENAME "glib-${GLIB_VERSION}.tar.xz"
-    SHA512 a068f2519cfb82de8d4b7f004e7c1f15e841cad4046430a83b02b359d011e0c4077cdff447a1687ed7c68f1a11b4cf66b9ed9fc23ab5f0c7c6be84eb0ddc3017)
+set(GLIB_VERSION 2.60.4)
 
-vcpkg_extract_source_archive_ex(
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.gnome.org
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
+    REPO GNOME/glib
     REF ${GLIB_VERSION}
-    PATCHES
-        use-libiconv-on-windows.patch
-        arm64-defines.patch
-        fix-arm-builds.patch
+    SHA512 433d9e476fdf5db8cb3a8740227e91da4b9d23bfe233e18df983a5089a77e810b1458e2d7e2cba4a4f5b2b6ec7ccd56fde1660b024268b66f03f67d6e1edf3ed
+    HEAD_REF glib-2-60
 )
 
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/cmake DESTINATION ${SOURCE_PATH})
-file(REMOVE_RECURSE ${SOURCE_PATH}/glib/pcre)
-file(WRITE ${SOURCE_PATH}/glib/pcre/Makefile.in)
-file(REMOVE ${SOURCE_PATH}/glib/win_iconv.c)
-
-vcpkg_configure_cmake(
+vcpkg_configure_meson(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
-        -DGLIB_VERSION=${GLIB_VERSION}
-    OPTIONS_DEBUG
-        -DGLIB_SKIP_HEADERS=ON
-        -DGLIB_SKIP_TOOLS=ON
+        --backend=ninja
 )
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-glib TARGET_PATH share/unofficial-glib)
-
+vcpkg_install_meson()
 vcpkg_copy_pdbs()
-vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/glib)
 
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/glib)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/glib/COPYING ${CURRENT_PACKAGES_DIR}/share/glib/copyright)
+# Move executables from bin to tools/glib
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/glib)
+
+set(BINTOOLS
+    gdbus
+    gio
+    gio-querymodules
+    glib-compile-resources
+    glib-compile-schemas
+    gobject-query
+    gresource
+    gsettings
+    gspawn-win64-helper
+    gspawn-win64-helper-console
+)
+foreach(bintool ${BINTOOLS})
+    if(CMAKE_HOST_WIN32)
+        file(RENAME ${CURRENT_PACKAGES_DIR}/bin/${bintool}.exe ${CURRENT_PACKAGES_DIR}/tools/glib/${bintool}.exe)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/${bintool}.pdb)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/${bintool}.exe)
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/${bintool}.pdb)
+    else()
+        file(RENAME ${CURRENT_PACKAGES_DIR}/bin/${bintool} ${CURRENT_PACKAGES_DIR}/tools/glib/${bintool})
+        file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/${bintool})
+    endif()
+endforeach()
+
+set(PYTOOLS
+    gdbus-codegen
+    glib-genmarshal
+    glib-mkenums
+    gtester-report
+)
+foreach(pytool ${PYTOOLS})
+    if(CMAKE_HOST_WIN32)
+        set(pytool ${pytool}.py)
+    endif()
+
+    file(RENAME ${CURRENT_PACKAGES_DIR}/bin/${pytool} ${CURRENT_PACKAGES_DIR}/tools/glib/${pytool})
+    file(REMOVE ${CURRENT_PACKAGES_DIR}/debug/bin/${pytool})
+endforeach()
+
+# Move include files one level higher up as customary for vcpkg installation
+file(COPY ${CURRENT_PACKAGES_DIR}/include/glib-2.0/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/glib-2.0)
+if(CMAKE_HOST_WIN32)
+    file(COPY ${CURRENT_PACKAGES_DIR}/include/gio-win32-2.0/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/gio-win32-2.0)
+else()
+    file(COPY ${CURRENT_PACKAGES_DIR}/include/gio-unix-2.0/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/gio-unix-2.0)
+endif()
+file(COPY ${CURRENT_PACKAGES_DIR}/lib/glib-2.0/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/glib-2.0)
+
+# Remove bin directory
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
+
+# Remove debug/share directory
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/glib RENAME copyright)
