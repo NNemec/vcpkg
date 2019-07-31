cbcf7fa4acd553c72b495362d96a68dad78be8b2
Norbert Nemec <Norbert@Nemec-online.de>
2019-07-31 13:17:44 +0100

    [glib] pass python PATH through to meson

--- ports/glib/portfile.cmake
+++ ports/glib/portfile.cmake
@@ -21,6 +21,10 @@ vcpkg_from_gitlab(
     HEAD_REF glib-2-60
 )
 
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
+vcpkg_add_to_path("${PYTHON3_DIR}")
+
 vcpkg_configure_meson(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
