93686ed3a77d3b8ce0e5651834fcde25cd9838bd
Norbert Nemec <Norbert@Nemec-online.de>
2019-08-03 19:35:51 +0200

    [atk] upgrade to 2.33.3 (mesonbuild)

--- ports/atk/portfile.cmake
+++ ports/atk/portfile.cmake
@@ -2,34 +2,29 @@ include(vcpkg_common_functions)
 
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY ONLY_DYNAMIC_CRT)
 
-set(ATK_VERSION 2.24.0)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/atk-${ATK_VERSION})
-vcpkg_download_distfile(ARCHIVE
-    URLS "http://ftp.gnome.org/pub/GNOME/sources/atk/2.24/atk-${ATK_VERSION}.tar.xz"
-    FILENAME "atk-${ATK_VERSION}.tar.xz"
-    SHA512 3ae0a4d5f28d5619d465135c685161f690732053bcb70a47669c951fbf389b5d2ccc5c7c73d4ee8c5a3b2df14e2f5b082e812a215f10a79b27b412d077f5e962)
+set(ATK_VERSION 2.32.0)
 
-vcpkg_extract_source_archive(${ARCHIVE})
-vcpkg_apply_patches(SOURCE_PATH ${SOURCE_PATH}/atk
-    PATCHES
-        ${CMAKE_CURRENT_LIST_DIR}/fix-encoding.patch)
-
-file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.gnome.org
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO GNOME/atk
+    REF ATK_2_33_3
+    SHA512 4dc29d2f795763b6be3c7b4038566f7603945f3e91b3c1791a4ad350ec484ad672c495836d5e6e9b9633c0c768a2ac1dd8793a3e381ce0e26364ef115a770c86
+    HEAD_REF master
+)
 
 vcpkg_find_acquire_program(PYTHON3)
 get_filename_component(PYTHON3_DIR "${PYTHON3}" DIRECTORY)
 vcpkg_add_to_path("${PYTHON3_DIR}")
 
-vcpkg_configure_cmake(
+vcpkg_configure_meson(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA
     OPTIONS
-        -DCMAKE_PROGRAM_PATH=${CURRENT_INSTALLED_DIR}/tools/glib
-    OPTIONS_DEBUG
-        -DATK_SKIP_HEADERS=ON)
+        --backend=ninja
+)
 
-vcpkg_install_cmake()
+vcpkg_install_meson()
 vcpkg_copy_pdbs()
 
-file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/atk)
-file(RENAME ${CURRENT_PACKAGES_DIR}/share/atk/COPYING ${CURRENT_PACKAGES_DIR}/share/atk/copyright)
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/atk RENAME copyright)
